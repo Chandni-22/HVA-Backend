9ffbba4a5f833bb08bc631c19b1c4ae8da23910e
Chandni-22 <chandni22@navgurukul>
2024-08-24 00:12:39 +0530

    5 loop questions done

---
+++ Coding-tasks/2-Loops.py
@@ -0,0 +1,64 @@
+'''1.) N times "Hello World": You are given an integer n. Print "Hello World" n times.
+Sample Input: 
+5
+Sample Output: 
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World'''
+# n=int(input("Enter a number: "))
+# i=1
+# while i<=n:
+#     print("Hello World")
+#     i+=1
+
+
+'''2.) N times * in same line: You are given an integer n. Print * n times in the same line with space in between.
+Sample Input: 
+5
+Sample Output: 
+* * * * * '''
+# n=int(input("Enter a number: "))
+# i=1
+# while i<=n:
+#     print("*",end=" ")
+#     i+=1
+
+
+'''3.) First n Natural Numbers: You are given an integer n. Print the first n natural numbers.
+Sample Input: 
+7
+Sample Output: 
+1 2 3 4 5 6 7'''
+# n=int(input("Enter a number: "))
+# i=1
+# while i<=n:
+#     print(i,end=" ")
+#     i+=1
+
+
+'''4.) All Even Numbers till n: You are given an integer n. Print all the even numbers less than equal to n.
+Sample Input: 
+14
+Sample Output: 
+2 4 6 8 10 12 14'''
+# n=int(input("Enter a number: "))
+# i=2
+# while i<=n:
+#     print(i,end=" ")
+#     i+=2
+
+
+'''5.) All Square Numbers till n: You are given an integer n. Print all the square numbers less than equal to n.
+Sample Input: 
+50
+Sample Output: 
+1 4 9 16 25 36 49'''
+# n=int(input("Enter a number: "))
+# i=1
+# while i<=n:
+#     a=i**2
+#     if a<=n:
+#         print(a,end=" ")
+#     i+=1
