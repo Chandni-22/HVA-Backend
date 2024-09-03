2cf7998ed49845da85316b51dbff80a9ab7f62b3
Chandni-22 <chandni22@navgurukul>
2024-09-04 04:08:12 +0530

    Loop done

--- Coding-tasks/3-Advanced-loops.py
+++ Coding-tasks/3-Advanced-loops.py
@@ -51,3 +51,198 @@ Sample Output:
 # HCF=a
 # LCM=(num1*num2)//HCF
 # print(LCM)
+
+
+'''5.) Binary to Decimal: Write a program that takes a Binary Number B as input and prints the Decimal equivalent of the given input.
+Sample Input: 
+1000
+Sample Output: 
+8'''
+# binary=int(input("Enter binary number: "))
+# decimal=0
+# i=0
+# while binary>0:
+#     decimal+=(binary%10)*(2**i)
+#     i+=1
+#     binary=binary//10
+# print(decimal)
+
+
+'''6.) Decimal to Binary: Write a program that takes a Decimal Number D as input and prints the Binary equivalent of the given input.
+Sample Input: 
+8
+Sample Output: 
+1000'''
+# decimal=int(input("Enter decimal number: "))
+# binary=""
+# while decimal>0:
+#     binary+=str(decimal%2)
+#     decimal=decimal//2
+# binary=int(binary[::-1])
+# print(binary)
+
+
+'''7.) Is Prime?: You are given an integer n. Print yes, if it is a prime number. Print no, if it is not a prime number.
+Sample Input 1: 
+49
+Sample Output 1: 
+No
+Sample Input 2: 
+37
+Sample Output 2: 
+Yes'''
+# n=int(input("Enter numbers: "))
+# if (n<2) or (n>2 and n%2==0):
+#     prime="No"
+# elif n==2:
+#     prime="Yes"
+# else:
+#     i=2
+#     while i<=(n//2):
+#         if n%i==0:
+#             prime="No"
+#             break
+#         i+=1
+#     else:
+#         prime="Yes"    
+# print(prime)
+
+
+'''8.) Is Perfect Number?: You are given an integer n. Print yes, if it is a Perfect Number. Print no, if it is not a Perfect Number.
+A Perfect Number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
+Sample Input 1: 
+28
+Sample Output 1: 
+Yes
+Explanation 1: The factors of 28 are 1, 2, 4, 7, 14, 28. We don't consider the number itself. 28 = 1+2+4+7+14.
+Sample Input 2: 
+24
+Sample Output 2: 
+No
+Explanation 2: The factors of 24 are 1, 2, 3, 4, 6, 8, 12, 24. We don't consider the number itself. But 24 != 1+2+3+4+6+8+12.'''
+# n=int(input("Enter numbers: "))
+# i=1
+# sum=0
+# while i<=(n//2):
+#     if n%i==0:
+#         sum+=i
+#     i+=1
+# ans= "Yes" if sum==n else "No"
+# print(ans)
+
+
+'''9.) Is Armstrong Number?: You are given an integer n. Print yes, if it is an Armstrong Number. Print no, if it is not an Armstrong Number.
+An Armstrong Number is a positive number that equals the sum of its digits, each raised to the power of the number of digits.
+Sample Input 1: 
+1634
+Sample Output 1: 
+Yes
+Explanation 1: 1634 has 4 digits, so we raise each digit to the power of 4. 1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634 
+Sample Input 2: 
+153
+Sample Output 2: 
+Yes
+Explanation 2: 153 has 3 digits, so we raise each digit to the power of 3. 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153'''
+# n=int(input("Enter numbers: "))
+# a=n
+# l=len(str(n))
+# sum=0
+# while a>0:
+#     sum+=(a%10)**l
+#     a=a//10
+# ans= "Yes" if sum==n else "No"
+# print(ans)
+
+
+'''10.) Star Pattern 1: Print the following pattern based on the given input.
+Sample Input: 
+6
+Sample Output: 
+*
+**
+***
+****
+*****
+******
+Explanation: Since the input is 6, it prints a total of 6 lines. In each line, the star count increases.'''
+# n=int(input("Enter numbers: "))
+# i=1
+# while i<=n:
+#     print("*"*i)
+#     i+=1
+
+
+'''11.) Star Pattern 2: Print the following pattern based on the given input.
+Sample Input: 
+6
+Sample Output: 
+     *
+    **
+   ***
+  ****
+ *****
+******
+Explanation: Since the input is 6, it prints a total of 6 lines. In each line, the star count increases and the leading space decreases.'''
+# n=int(input("Enter numbers: "))
+# i=1
+# while i<=n:
+#     print(" "*(n-i),end="")
+#     print("*"*i)
+#     i+=1
+
+
+'''12.) Star Pattern 3:
+Print the following pattern based on the given input.
+Sample Input: 
+6
+Sample Output: 
+*
+ *
+  *
+   *
+    *
+     *
+Explanation: Since the input is 6, it prints a total of 6 lines. In each line, it prints only one star, but the leading space increases.'''
+# n=int(input("Enter numbers: "))
+# i=0
+# while i<n:
+#     print(" "*i,end="")
+#     print("*")
+#     i+=1
+
+
+'''13.) Star Pattern 4: Print the following pattern based on the given input.
+Sample Input: 
+5
+Sample Output: 
+    *
+   ***
+  *****
+ *******
+*********
+Explanation: Since the input is 5, it prints a total of 5 lines of stars. Each line has stars as well as leading spaces. In each line, the star count increases by 2, but the leading space decreases by 1.'''
+# n=int(input("Enter numbers: "))
+# i=1
+# while i<=n:
+#     print(" "*(n-i)+"*"*((2*i)-1))
+#     i+=1
+
+
+'''14.) First n prime numbers: You are given an integer n. Print the first n prime numbers.
+Sample Input: 
+7
+Sample Output: 
+2 3 5 7 11 13 17'''
+# n=int(input("Enter numbers: "))
+# i=2
+# count=0
+# while count<n:
+#     j=2
+#     while j<=(i//2):
+#         if i%j==0:
+#             break
+#         j+=1
+#     else:
+#         print(i,end=" ")
+#         count+=1
+#     i+=1
